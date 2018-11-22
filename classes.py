bb68f5ddab50fa6c042ff1781a0c24ec8469b1cb
daniyal ahmed <Daniahmedkhatri@gmail.com>
2018-11-23 01:34:33 +0500

    Added py files

---
+++ classes.py
@@ -0,0 +1,9 @@
+class point:
+    def __init__(self,x,y):
+        self.x = x
+        self.y = y
+
+
+p = point(2,3)
+print(p.x)
+print(p.y)
---
+++ variables.py
@@ -0,0 +1,16 @@
+i = 28
+# print('i is ' + str(i))
+
+print(f'I is {i}')
+
+f = 2.1
+
+print(f'F is {f}')
+
+b = True
+
+print(f'B is {b}')
+
+N = None
+print(f"N is {N}")
+
